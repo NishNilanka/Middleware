416a87a54e63477c486adc4f8d0eb3bedae433c6
Nishan Gunawardena <nishan.nilanka@gmail.com>
2015-05-22 00:26:32 +0530

    Server starting code is added. used the TCP connection

--- ServerOne/src/serverone/ServerOne.java
+++ ServerOne/src/serverone/ServerOne.java
@@ -4,6 +4,14 @@
  */
 package serverone;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  *
  * @author Nishan Gunawardena
@@ -14,6 +22,36 @@ public class ServerOne {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            ServerSocket server = new ServerSocket(3074);
+            System.err.println("Server is listening to port : 3074");
+            Socket accept = server.accept();
+            InetAddress InetAddress = accept.getInetAddress();
+            System.err.println(InetAddress + " get connected...");
+            BufferedReader bf = new BufferedReader(new InputStreamReader(accept.getInputStream()));
+            PrintWriter print = new PrintWriter(accept.getOutputStream(), true);
+            String Str;
+            while (true) {
+
+                Str = bf.readLine();
+                System.out.println("Client - " + InetAddress + " : " + Str);
+
+                if (Str.equals("Bye")) {
+                    break;
+                }
+
+
+                print.println(Str);
+            }
+            System.err.println("Connection terminated");
+            print.close();
+            bf.close();
+            accept.close();
+
+        } catch (IOException ex) {
+            System.err.println(ex);
+        }
+
+
     }
 }
